123d95aca245dbd6ee31e7e68fdcaf489a1a8f11
Germiprogramer <llorentemunozgerman@gmail.com>
2023-11-21 10:26:36 +0100

    tratando de implementar la gui

--- ejercicio1/composite/composite.py
+++ ejercicio1/composite/composite.py
@@ -142,7 +142,7 @@ class CompositeMenuCompuesto(Component):
             #empleamos slicing para eliminar los ultimos 3 caracteres de cada string y añadimos al csv
             writer.writerow([self.code, self.nombre, self.discount, pizza[:-3], entrante[:-3], bebida[:-3], postre[:-3]])
         
-
+'''
 menu_infantil = CompositeMenu(1, "MENU_INFANTIL", discount=0.1)
 menu_infantil.add_component(Pizza_Component("Pizza Infantil"))
 menu_infantil.add_component(Entrante_Component("Patatas fritas", 3.0))
@@ -158,6 +158,53 @@ print(menu_compuesto.decir_precio())
 
 menu_infantil.to_csv()
 menu_compuesto.to_csv()
+'''
+
+import tkinter as tk
+from tkinter import messagebox
+
+class PizzeriaMenuApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Pizzería Menú")
+        
+        self.menu_infantil = CompositeMenu(1, "MENU_INFANTIL", discount=0.1)
+        self.menu_infantil.add_component(Pizza_Component("Pizza Infantil"))
+        self.menu_infantil.add_component(Entrante_Component("Patatas fritas", 3.0))
+        self.menu_infantil.add_component(Bebida_Component("Refresco", 2.0))
+        self.menu_infantil.add_component(Postre_Component("Helado", 2.0))
+
+        self.menu_compuesto = CompositeMenuCompuesto(2,"MENU_COMPUESTO", discount=0.1)
+        self.menu_compuesto.add_component(self.menu_infantil)
+        self.menu_compuesto.add_component(self.menu_infantil)
+
+        self.create_gui()
+
+    def create_gui(self):
+        # Etiqueta
+        label = tk.Label(self.master, text="¡Bienvenido a la Pizzería!")
+        label.pack(pady=10)
+
+        # Botón para pedir el menú infantil
+        button_menu_infantil = tk.Button(self.master, text="Pedir Menú Infantil", command=self.pedir_menu_infantil)
+        button_menu_infantil.pack(pady=5)
+
+        # Botón para pedir el menú compuesto
+        button_menu_compuesto = tk.Button(self.master, text="Pedir Menú Compuesto", command=self.pedir_menu_compuesto)
+        button_menu_compuesto.pack(pady=5)
+
+    def pedir_menu_infantil(self):
+        precio = self.menu_infantil.decir_precio()
+        messagebox.showinfo("Pedido Realizado", f"Has pedido el Menú Infantil. Precio: ${precio}")
+
+    def pedir_menu_compuesto(self):
+        precio = self.menu_compuesto.decir_precio()
+        messagebox.showinfo("Pedido Realizado", f"Has pedido el Menú Compuesto. Precio: ${precio}")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PizzeriaMenuApp(root)
+    root.mainloop()
 
 
 
