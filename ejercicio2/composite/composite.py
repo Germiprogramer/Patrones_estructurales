baf08230b74f77adc396e1785c05b1169ce62c41
Germiprogramer <llorentemunozgerman@gmail.com>
2023-11-26 21:34:32 +0100

    commit

--- ejercicio2/composite/composite.py
+++ ejercicio2/composite/composite.py
@@ -39,11 +39,73 @@ class Enlace_Component(Component):
     def operacion(self):
         print(f"Enlace: {self.nombre}, Tamaño: {self.tamanio}, Enlace: {self.enlace}")
 
-class Carpeta(Component):
+class Carpeta_Cima(Component):
     def __init__(self, nombre: str) -> None:
         self.nombre = nombre
         self.components = []
         self.tamanio = 0
+    
+    def decir_tamanio(self) -> float:
+        return self.tamanio
+
+    def decir_nombre(self) -> str:
+        return self.nombre
+
+    def operacion(self):
+        print(f"Carpeta: {self.nombre}, Tamaño: {self.tamanio}")
+        for component in self.components:
+            component.operacion()
+
+    def add_component(self, component: Component) -> None:
+        self.components.append(component)
+        self.tamanio += component.decir_tamanio()
+
+
+    def remove_component(self, component: Component) -> None:
+        self.components.remove(component)
+        self.tamanio -= component.decir_tamanio()
+
+    def list_components(self) -> None:
+        for component in self.components:
+            print(f"\t{component.decir_nombre()}")
+
+    def reconstruccion(self):
+        with open("ejercicio2/datos/registro_de_operaciones.csv", 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+
+                operacion = row["operacion"]
+                fecha = row["fecha"]
+                nombre = row["nombre"]
+                tamanio = float(row["tamanio"])
+                tipo = row["tipo"]
+                enlace = row["enlace"]
+
+                if operacion == "crear documento":
+                    self.add_component(Documento_Component(nombre, tamanio, tipo))
+                elif operacion == "crear enlace":
+                    self.add_component(Enlace_Component(nombre, tamanio, enlace))
+                elif operacion == "crear carpeta":
+                    self.add_component(Carpeta(nombre, None))
+                elif operacion == "eliminar":
+                    self.remove_component(nombre)
+                elif operacion == "buscar":
+                    pass
+                elif operacion == "modificar":
+                    self.modificar(nombre, tamanio)
+                elif operacion == "acceder":
+                    pass
+                else:
+                    print("Operación no válida")
+
+
+
+class Carpeta(Component):
+    def __init__(self, nombre: str, datos) -> None:
+        self.nombre = nombre
+        self.components = []
+        self.tamanio = 0
+        self.datos = datos
     
     def decir_tamanio(self) -> float:
         return self.tamanio
@@ -108,7 +170,7 @@ class Proxy(Component):
             self.crear_documento(nombre, tamanio, tipo)
         elif operacion == "crear enlace":
             nombre = input("Ingrese el nombre del enlace: ")
-            tamanio = float(input("Ingrese el tamaño del enlace: "))
+            tamanio = 0
             enlace = input("Ingrese el enlace: ")
             self.crear_enlace(nombre, tamanio, enlace)
         elif operacion == "crear carpeta":
@@ -165,15 +227,11 @@ class Proxy(Component):
                 
                 proxy = Proxy(component)
                 
-        
-            
-
-    
-
 
 if __name__ == "__main__":
 
-    carpeta1 = Carpeta("Carpeta 1")
+    carpeta1 = Carpeta_Cima("Carpeta 1")
+    carpeta1.reconstruccion()
     carpeta1.add_component(Documento_Component("Documento 1", 10, "docx"))
     carpeta1.add_component(Documento_Component("Documento 2", 20, "docx"))
     carpeta1.add_component(Documento_Component("Documento 3", 30, "docx"))
